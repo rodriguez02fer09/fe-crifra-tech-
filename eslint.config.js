5b283431a47e6c1cfb5b5dc1f1197bcf3de19609
Fernanda Rodriguez <fernandarpy@gmail.com>
2025-11-19 20:01:38 -0500

    chrore-Eslint prettirt

--- eslint.config.js
+++ eslint.config.js
@@ -3,15 +3,15 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import prettierConfig from 'eslint-config-prettier'
 
-export default defineConfig([
-  globalIgnores(['dist']),
+export default tseslint.config(
+  { ignores: ['dist'] },
   {
     files: ['**/*.{ts,tsx}'],
     extends: [
       js.configs.recommended,
-      tseslint.configs.recommended,
+      ...tseslint.configs.recommended,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
     ],
@@ -20,4 +20,5 @@ export default defineConfig([
       globals: globals.browser,
     },
   },
-])
+  prettierConfig
+)
